29f6aef9a7d0b184505a42703979980415aeba1a
CrazyPythonCoder <aaravsonaniatpython@gmail.com>
2022-08-25 22:31:19 +0100

    Ininial commit

--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is Aarav's website.")
+    return HttpResponse("<title>Aarav's Website</title> This is Aarav's website.")
